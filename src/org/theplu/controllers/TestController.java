a412dfc045300ac2cd7fc46c03b2b95cd5fb6dc6
Vuk Bozovic <vbozovic14@raf.rs>
2018-05-28 17:50:11 +0200

    Res endpoint

--- src/org/theplu/controllers/TestController.java
+++ src/org/theplu/controllers/TestController.java
@@ -2,7 +2,9 @@ package org.theplu.controllers;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -12,10 +14,11 @@ import javax.ws.rs.Produces;
 public class TestController {
 
 	
-    @GET
+    @POST
     @Produces("text/json")
-	public String getAll(){
-		return "Hello";
+    @Consumes("application/json")
+	public String getAll(TestInput input){
+		return "Hello "+input.getMessage();
 	}
 	
 }
